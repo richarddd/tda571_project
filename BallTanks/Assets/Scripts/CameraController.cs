1137d2b34d3869ceb9afcdd1c97fd1f07a9c3a41
Emelie Svensson <sakuradoud@hotmail.com>
2015-01-28 16:28:48 +0100

    Made the camera follow the ball when it moves, and rotate around it depending on where the mouse is

---
+++ BallTanks/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	public Vector3 offset;
+	public float speed;
+	Vector3 point;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+	}
+
+	void Update () {
+		Vector3 pos = transform.position;
+		Vector3 dir = Input.mousePosition - pos;
+		float angle = Mathf.Atan2 (dir.x -Screen.width/2, dir.y) * Mathf.Rad2Deg;
+		 
+		point = player.transform.position;
+		Debug.Log (point);
+		transform.RotateAround (point, new Vector3 (0.0f, 1.0f, 0.0f), speed*Time.deltaTime*angle);
+
+	}
+
+}
+
