b31ff77a6f6b545ff7b0dc9f1e7ec50197194b26
Emelie Svensson <sakuradoud@hotmail.com>
2015-01-28 14:01:32 +0100

    Added first version of a rolling ball

---
+++ BallTanks/Assets/PlayerControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+		float moveHorizental = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizental, 0.0f, moveVertical);
+
+		rigidbody.AddForce(movement * speed);
+	}
+}
