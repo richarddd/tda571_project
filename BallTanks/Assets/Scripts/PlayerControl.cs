c8daf0a98cbf88fcd5d8398b083bdd820358c714
gej9 <gej9@hi.is>
2015-02-02 10:29:15 +0100

    Destructible walls with explosion and sound effects

--- BallTanks/Assets/Scripts/PlayerControl.cs
+++ BallTanks/Assets/Scripts/PlayerControl.cs
@@ -5,22 +5,26 @@ public class PlayerControl : MonoBehaviour {
 
 	public float speed;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 
 	void FixedUpdate() {
-		float moveHorizental = Input.GetAxis("Horizontal");
+		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
-		Vector3 movement = new Vector3 (moveHorizental, 0.0f, moveVertical);
-
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rigidbody.AddForce(movement * speed);
 	}
+
+	void OnCollisionEnter(Collision collision){
+
+		//If the ball collides with a wall, reverse the movement
+		if (collision.gameObject.tag == "Wall") {
+			ContactPoint cp = collision.contacts[0];
+			Vector3 oldVelocity = rigidbody.velocity;
+			rigidbody.velocity = oldVelocity + cp.normal * 1.0f * oldVelocity.magnitude;
+		}
+
+	}
+
+
 }
---
+++ BallTanks/Assets/Scripts/WallControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallControl : MonoBehaviour {
+
+	public GameObject explosion;
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.tag == "Player") {
+			gameObject.SetActive (false);
+			Instantiate(explosion, transform.position, transform.rotation);
+		}
+		
+	}
+
+}
