dbf5dc69b9171209c7722c735d256ba46c43a7db
Emelie Svensson <sakuradoud@hotmail.com>
2015-02-04 14:43:56 +0100

    First version of player dying

---
+++ BallTanks/Assets/Scripts/Killing.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Killing : MonoBehaviour {
+	private GameObject canvas;
+	// Use this for initialization
+	void Start () {
+		canvas =GameObject.FindGameObjectWithTag("Canvas");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider colInfo){
+		if (colInfo.tag == "Player") {
+			if(colInfo.networkView.isMine){
+				canvas.transform.GetChild(0).gameObject.SetActive(true);
+			}
+			Destroy(colInfo.gameObject);
+
+		}
+	}
+}
