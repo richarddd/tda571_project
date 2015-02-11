1a7b74651a497c09d1e92fc170a0530ffa02b9fc
Gustav Simonsson <gussim@student.chalmers.se>
2015-02-11 15:43:08 +0100

    Shooting without explosion

---
+++ BallTanks/Assets/Scripts/Shooter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+
+	public Rigidbody projectile;
+	public float shotForce = 1000f;
+	public float moveSpeed = 10f;
+
+	void Update () {
+		
+		if (Input.GetButtonUp ("Fire1")) {
+			Rigidbody shot = Instantiate (projectile, transform.position + (new Vector3(0.15f,0,1)), transform.rotation) as Rigidbody;
+			shot.AddForce (transform.up * -1 * shotForce);
+		}
+	}
+}
