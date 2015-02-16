ee76edb80882fc110bacecb92898d443d7736e63
Emelie Svensson <sakuradoud@hotmail.com>
2015-02-16 09:55:31 +0100

    the walls can now be destroyed by the projectile // gudrun made this

--- BallTanks/Assets/Scripts/WallControl.cs
+++ BallTanks/Assets/Scripts/WallControl.cs
@@ -6,7 +6,7 @@ public class WallControl : MonoBehaviour {
 	public GameObject explosion;
 
 	void OnCollisionEnter(Collision collision){
-		if (collision.gameObject.tag == "Player") {
+		if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Projectile") {
 			gameObject.SetActive (false);
 			Instantiate(explosion, transform.position, transform.rotation);
 		}
