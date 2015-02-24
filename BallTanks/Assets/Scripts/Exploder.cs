00f2e0af5b37c828058696d4831843b78bdd46fe
Emelie Svensson <sakuradoud@hotmail.com>
2015-02-24 11:08:40 +0100

    Updated projectile (need to add one line in fixed cannon after merge)

--- BallTanks/Assets/Scripts/Exploder.cs
+++ BallTanks/Assets/Scripts/Exploder.cs
@@ -5,17 +5,29 @@ public class Exploder : MonoBehaviour {
 
 	public float radius = 5.0f;
 	public float power = 10.0f;
+	private GameObject shooter;
 	
 	void OnCollisionEnter(Collision collision){
-		
 		Vector3 explosionPos = transform.position;
 		Collider[] colliders = Physics.OverlapSphere (explosionPos, radius);
-		
-		foreach (Collider hit in colliders) {
-			if (hit && hit.rigidbody) {
-				hit.rigidbody.AddExplosionForce (power, explosionPos, radius, 3.0f);
+
+		if (collision.gameObject != shooter) {
+			foreach (Collider hit in colliders) {
+				if (hit && hit.rigidbody) {
+					hit.rigidbody.AddExplosionForce (power, explosionPos, radius, 3.0f);
+					Destroy (gameObject, 0.05f);
+				}
+			}
+			if (collision.gameObject.tag == "Player") {
+
+				PlayerHealthBar hitPlayer = collision.gameObject.transform.parent.GetComponent<PlayerHealthBar>();
+				hitPlayer.decrementHealth(10);		
 			}
 		}
-		Destroy (gameObject, 0.05f);
+
+	}
+
+	public void setShooter(GameObject owner){
+		shooter = owner;
 	}
 }
