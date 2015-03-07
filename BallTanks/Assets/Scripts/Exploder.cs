3def2eba085ac8bb4e5d2509c84656835650b874
Gustav Simonsson <gussim@student.chalmers.se>
2015-03-07 11:59:51 +0100

    The explosion from projectile shots now damage opponents, the players take more damage if they are closer to the center of the explosion.

--- BallTanks/Assets/Scripts/Exploder.cs
+++ BallTanks/Assets/Scripts/Exploder.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Exploder : MonoBehaviour {
 
 	public float radius = 5.0f;
-	public float power = 10.0f;
+	public float power = 500.0f;
 	private GameObject shooter;
 	public GameObject explotion;
 	
@@ -16,18 +16,19 @@ public class Exploder : MonoBehaviour {
 			foreach (Collider hit in colliders) {
 				if (hit && hit.rigidbody) {
 					hit.rigidbody.AddExplosionForce (power, explosionPos, radius, 3.0f);
-					Destroy (gameObject, 0.05f);
-					SpawnExplotion();
-
+					Destroy (gameObject, 0.1f);
+					if (hit.tag.Equals ("Player")) {
+						
+						PlayerHealthBar hitPlayer = hit.gameObject.transform.parent.GetComponent<PlayerHealthBar>();
+						int damage = ((int) (10/radius)) * (((int) radius) - ((int) Vector3.Distance(explosionPos, hit.transform.position)));
+						if(damage > 0)
+						{
+							hitPlayer.decrementHealth(damage);
+						}
+					}
 				}
 			}
-			if (collision.gameObject.tag == "Player") {
-
-				PlayerHealthBar hitPlayer = collision.gameObject.transform.parent.GetComponent<PlayerHealthBar>();
-				hitPlayer.decrementHealth(10);		
-			}
 		}
-
 	}
 
 	private void SpawnExplotion(){
