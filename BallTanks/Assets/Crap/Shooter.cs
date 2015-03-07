0de6e7b09bd6f135da39979667f374edeb0d4bf5
gej9 <gej9@hi.is>
2015-03-07 14:46:38 +0100

    New crap folder

---
+++ BallTanks/Assets/Crap/Shooter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour
+{
+
+		
+
+		void OnSerializeNetworkView (BitStream stream, NetworkMessageInfo info)
+		{
+				
+				if (stream.isWriting) {
+						
+						
+				} else {
+						
+				}
+		}
+
+		
+
+		void Update ()
+		{
+
+				if (networkView.isMine) {
+		
+						
+				}
+		}
+}
