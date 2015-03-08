4d70db774499737e79e860a6204b9c2621a60ef9
gej9 <gej9@hi.is>
2015-03-08 10:32:22 +0100

    Volcano particle effects and sounds when player falls out of the arena

--- BallTanks/Assets/Scripts/Killing.cs
+++ BallTanks/Assets/Scripts/Killing.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 public class Killing : MonoBehaviour {
 	private GameObject canvas;
-	public GameObject OceanParticleEffect;
+	public GameObject ParticleEffect;
 	private int noOfPlayers=0;
 	// Use this for initialization
 	void Start () {
@@ -35,7 +35,7 @@ public class Killing : MonoBehaviour {
 	void OnTriggerEnter(Collider colInfo){
 		GameObject playerToKill = colInfo.gameObject;
 		if (colInfo.tag == "Player" && colInfo.transform.parent.networkView.isMine) {
-			Network.Instantiate(OceanParticleEffect, colInfo.transform.position, colInfo.transform.rotation,0);
+			Network.Instantiate(ParticleEffect, colInfo.transform.position, colInfo.transform.rotation,0);
 			audio.Play();
 			Kill (playerToKill);
 		}
